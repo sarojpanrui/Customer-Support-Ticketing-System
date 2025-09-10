ecc4454e2e380826c0ac7bedde22a61699c8ddc3
saroj <sarojpanrui@gmail.com>
2025-09-10 18:17:48 +0530

    setting login navigate

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  server: {
+    historyApiFallback: true // Only needed for local dev in some setups
+  }
+
 })
