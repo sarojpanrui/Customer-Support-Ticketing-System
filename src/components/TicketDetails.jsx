1823cc21d4b2fdb5418b3f24b998f6c3aa5bf2c4
saroj <sarojpanrui@gmail.com>
2025-09-11 12:59:06 +0530

    cursor pointer

--- src/components/TicketDetails.jsx
+++ src/components/TicketDetails.jsx
@@ -128,7 +128,7 @@ const TicketDetail = () => {
     <div className="p-6 max-w-2xl mx-auto merriweather">
       {/* Back Button */}
       <button
-        className="mb-6 px-5 py-2 bg-gray-200 hover:bg-gray-300 rounded-xl shadow-sm transition-all flex"
+        className="mb-6 px-5 py-2 bg-gray-200 hover:bg-gray-300 rounded-xl shadow-sm transition-all flex cursor-pointer"
         onClick={() => navigate(-1)}
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left-icon lucide-arrow-left"><path d="m12 19-7-7 7-7" /><path d="M19 12H5" /></svg>Back
