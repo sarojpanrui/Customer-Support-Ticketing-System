c5b3ddfdbf6f6f18f8833a7c34f81078c51e7aa7
saroj <sarojpanrui@gmail.com>
2025-09-12 12:59:01 +0530

    shadow

--- src/components/CustomerDashboard.jsx
+++ src/components/CustomerDashboard.jsx
@@ -101,7 +101,7 @@ const CustomerDashboard = () => {
             <select
               value={statusFilter}
               onChange={(e) => setStatusFilter(e.target.value)}
-              className="border px-3 py-2 rounded-lg shadow-xl border-gray-300"
+              className="border px-3 py-2 rounded-lg shadow-sm border-gray-300"
             >
               <option value="All">All Statuses</option>
               <option value="Open">Open</option>
