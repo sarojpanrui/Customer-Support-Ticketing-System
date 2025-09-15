4c15120a1b8080f318d0f802d0d3ccf837559dbb
saroj <sarojpanrui@gmail.com>
2025-09-15 12:29:44 +0530

    dec padding in nav of admin

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -98,7 +98,7 @@ const AdminDashboard = () => {
               </p>
 
               {/* Icon */}
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-play-icon lucide-play"><path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z" /></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-play-icon lucide-play"><path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z" /></svg>
 
 
             </div>
@@ -119,8 +119,8 @@ const AdminDashboard = () => {
               {/* Icon */}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="28"
-                height="28"
+                width="20"
+                height="20"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
